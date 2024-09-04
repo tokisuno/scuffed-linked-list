74136e975d8668fdf6a4d3bf7819156bbdcbbd97
tokisuno <lucaswmei@gmail.com>
2024-09-04 10:36:02 -0400

    finished with extras!

--- main.js
+++ main.js
@@ -11,7 +11,54 @@ class LinkedList {
         }
     }
     prepend(value) {
-        
+        const tempList = this.list;
+        this.list = [];
+        this.append(value);
+        tempList.forEach(item => {
+            this.append(item.value);
+        });
+    }
+    size() {
+        let size = 0;
+        for (const _ of Object.values(this.list)) size++;
+        return size;
+    }
+    head() {
+        return this.list[0];
+    }
+    tail() {
+        return this.list[this.list.length - 1];
+    }
+    at(index) {
+        return this.list[index];
+    }
+    pop() {
+        this.list = this.list.slice(0, this.list.length-1);
+        this.list[this.list.length - 1].next = null;
+        return this.list;
+    }
+    contains(value) {
+        for (const item of Object.values(this.list)) {
+            if (item.value === value) {
+                return true;
+            } else {
+                continue;
+            }
+        }
+        return false;
+    }
+    find(value) {
+        if (this.contains(value) === true) {
+            let pos = 0;
+            for (const item of Object.values(this.list)) {
+                if (item.value !== value) {
+                    pos++;
+                    continue;
+                }
+                return pos; 
+            }
+        }
+        console.log(pos)
     }
     toString() {
         let string = "";
@@ -20,6 +67,23 @@ class LinkedList {
         }
         string += "null";
         return string;
+    }
+    insertAt(value, index) {
+        const lhs = new LinkedList(this.list.slice(0, index));
+        const rhs = new LinkedList(this.list.slice(index, this.list.length));
+        
+        lhs.append(value)
+        for (const item of Object.values(rhs.list)) {
+            lhs.append(item.value);
+        }
+        this.list = lhs.list;
+    }
+    removeAt(index) {
+        const lhs = new LinkedList(this.list.slice(0, index)); 
+        const rhs = new LinkedList(this.list.slice(index+1, this.list.length));
+        
+        for (const item of Object.values(rhs.list)) lhs.append(item.value);
+        this.list = lhs.list;
     }
 }
 
@@ -35,5 +99,11 @@ list.append(12);
 list.append(14);
 list.append(15);
 list.append(23);
+list.prepend(100);
 
+list.insertAt(69, 2);
 console.log(list.toString());
+
+list.removeAt(4);
+console.log(list.toString());
+
